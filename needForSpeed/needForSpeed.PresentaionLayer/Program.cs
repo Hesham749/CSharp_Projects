46a8bc1bc10fdf0ba80334f574ca494b11f9d1b1
Hesham Elsayed <heesham749@gmail.com>
2024-12-11 23:44:17 +0200

    u

--- needForSpeed/needForSpeed.PresentaionLayer/Program.cs
+++ needForSpeed/needForSpeed.PresentaionLayer/Program.cs
@@ -6,13 +6,13 @@ namespace needForSpeed.PresentaionLayer
         static void Main(string[] args)
         {
 
-            clsPerformanceCar c1 = new("koko", "1994", 2010, 213, 20, 3, 300);
-            Console.WriteLine(c1.model);
-            clsShowCar c2 = new clsShowCar("hesham", "f4", 2002, 123, 33, 44, 700);
-            Console.WriteLine(c2.stars);
-            clsRace r1 = new clsRace(200, "El menea", 33);
-            r1.praticipants.Add(c1);
-            Console.WriteLine(r1.praticipants[0].brand);
+            //clsPerformanceCar c1 = new("koko", "1994", 2010, 213, 20, 3, 300);
+            //Console.WriteLine(c1.model);
+            //clsShowCar c2 = new clsShowCar("hesham", "f4", 2002, 123, 33, 44, 700);
+            //Console.WriteLine(c2.stars);
+            //clsRace r1 = new clsRace(200, "El menea", 33);
+            //r1.praticipants.Add(c1);
+            //Console.WriteLine(r1.praticipants[0].brand);
 
         }
     }
